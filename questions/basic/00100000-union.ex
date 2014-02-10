74d9857425a008aa4690f7827b396ef7e3863fba
talam <tanviralam2@gmail.com>
2014-02-09 19:24:26 -0500

    Updated some of the language for the Basic and Joins sections, fixed some typos

--- questions/basic/00100000-union.ex
+++ questions/basic/00100000-union.ex
@@ -3,13 +3,13 @@ Combining results from multiple queries
 |QUESTION|
 You, for some reason, want a combined list of all surnames and all facility names.  Yes, this is a contrived example :-).  Produce that list!
 |QUERY|
-select distinct surname 
+select surname 
 	from cd.members
 union all
-select distinct name
+select name
 	from cd.facilities;
 |ANSWER|
-<p>The <c>UNION</c> operator does what you might expect: combines two tables into one.  The caveat is that both tables must have the same number of columns and compatible data types.
+<p>The <c>UNION</c> operator does what you might expect: combines the results of two SQL queries into a single table.  The caveat is that both results from the two queries must have the same number of columns and compatible data types.
 <p><c>UNION</c> removes duplicate rows, while <c>UNION ALL</c> does not.  Use <c>UNION ALL</c> by default, unless you care about duplicate results.
 |HINT|
 Look up the SQL keyword <c>UNION</c>
