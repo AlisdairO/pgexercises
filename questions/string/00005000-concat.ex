5afd6e8f88957c239d6123d07bff8123d9d048c8
Alisdair Owens <awo101@zepler.net>
2014-08-01 22:21:19 +0100

    Add string concatenation exercise

---
+++ questions/string/00005000-concat.ex
@@ -0,0 +1,14 @@
+|QUESTIONNAME|
+Format the names of members
+|QUESTION|
+Output the names of all members, formatted as 'Surname, Firstname'
+|QUERY|
+select surname || ', ' || firstname as name from cd.members
+|ANSWER|
+Building strings in sql is similar to other languages, with the exception of the concatenation operator: ||.  Some systems (like SQL Server) use +, but || is the SQL standard.
+|HINT|
+Use the <c>||</c> operator to concatenate strings
+|SORTED|
+0
+|PAGEID|
+1e806c68-1974-11e4-aa6a-efecb22b3579
