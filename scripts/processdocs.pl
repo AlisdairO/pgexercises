41d851fb65bddb39187e39bdd3fbfac2ddd4a72a
Alisdair Owens <awo101@zepler.net>
2014-11-16 14:55:35 +0000

    Added arrays section

--- scripts/processdocs.pl
+++ scripts/processdocs.pl
@@ -5,7 +5,7 @@ use File::Find;
 my $outputloc = "../site/website/questions/";
 my $templateloc = "../site/website/questions/template.html";
 my $questionsloc = "../questions/";
-my @dirs = ("basic", "joins", "aggregates", "date", "string", "recursive");
+my @dirs = ("basic", "joins", "aggregates", "date", "string", "recursive", "arrays");
 #categories data structure ends up like: category -> files -> file metadata
 my $categories = {};
 my $emptyfile = {'title' => '', 'filenamehtml' => '#'};
