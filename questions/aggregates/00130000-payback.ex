80f43a090fb2bf02c25214dfce5ddcbf80e232f9
Alisdair Owens <alisdair@zaltys.net>
2016-07-09 14:40:14 +0100

    Fix column name on payback exercise

--- questions/aggregates/00130000-payback.ex
+++ questions/aggregates/00130000-payback.ex
@@ -7,7 +7,7 @@ select 	facs.name as name,
 	facs.initialoutlay/((sum(case
 			when memid = 0 then slots * facs.guestcost
 			else slots * membercost
-		end)/3) - facs.monthlymaintenance) as revenue
+		end)/3) - facs.monthlymaintenance) as months
 	from cd.bookings bks
 	inner join cd.facilities facs
 		on bks.facid = facs.facid
