51fc1e0414aac4c2bae6e3efe83e99037787d562
MrPowers <matthewkevinpowers@gmail.com>
2018-02-17 18:46:26 -0500

    Add the faculty ID where requirements that were omitted from the question

--- questions/joins/00020000-simplejoin2.ex
+++ questions/joins/00020000-simplejoin2.ex
@@ -1,7 +1,7 @@
 |QUESTIONNAME|
 Work out the start times of bookings for tennis courts
 |QUESTION|
-How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'?  Return a list of start time and facility name pairings, ordered by the time.
+How can you produce a list of the start times for bookings for tennis courts (facility IDs 0 or 1), for the date '2012-09-21'?  Return a list of start time and facility name pairings, ordered by the time.
 |QUERY|
 select bks.starttime as start, facs.name as name
 	from 
