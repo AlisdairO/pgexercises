424b76c692d5c304ad1866c122164adcb00d3ad9
Andrew Polukhin <qq7039108@gmail.com>
2021-09-07 21:03:22 +0300

    RankMembers - cleaner solution

--- questions/aggregates/00100000-rankmembers.ex
+++ questions/aggregates/00100000-rankmembers.ex
@@ -4,8 +4,8 @@ Rank members by (rounded) hours used
 Produce a list of members (including guests), along with the number of hours they've booked in facilities, rounded to the nearest ten hours.  Rank them by this rounded figure, producing output of first name, surname, rounded hours, rank.  Sort by rank, surname, and first name.
 |QUERY|
 select firstname, surname,
-	((sum(bks.slots)+10)/20)*10 as hours,
-	rank() over (order by ((sum(bks.slots)+10)/20)*10 desc) as rank
+	round(sum(bks.slots) / 2, -1) as hours,
+	rank() over (order by round(sum(bks.slots) / 2, -1) desc) as rank
 
 	from cd.bookings bks
 	inner join cd.members mems
@@ -15,14 +15,14 @@ order by rank, surname, firstname;
 |ANSWER|
 <p>This answer isn't a great stretch over our previous exercise, although it does illustrate the function of <c>RANK</c> better.  You can see that some of the clubgoers have an equal rounded number of hours booked in, and their rank is the same.  If position 2 is shared between two members, the next one along gets position 4.  There's a different function, <c>DENSE_RANK</c>, that would assign that member position 3 instead.</p>
 
-<p>It's worth noting the technique we use to do rounding here.  Adding 5, dividing by 10, and multiplying by 10 has the effect (thanks to integer arithmetic cutting off fractions) of rounding a number to the nearest 10.  In our case, because slots are half an hour, we need to add 10, divide by 20, and multiply by 10.  One could certainly make the argument that we should do the slots -> hours conversion independently of the rounding, which would increase clarity.</p>
+<p>It's worth noting the technique we use to do rounding here.  The <c>ROUND</c> function's second parameter is an integer that determines the number of decimal places after rounding. This way, specifying -1 works exactly as we expect: rounds to the nearest 10.</p>
 
-<p>Talking of clarity, this rounding malarky is starting to introduce a noticeable amount of code repetition.  At this point it's a judgement call, but you may wish to factor it out using a subquery as below:</p>
+<p>Talking of clarity, we can reduce the amount of code repetition.  At this point it's a judgement call, but you may wish to factor it out using a subquery as below:</p>
 
 <sql>
 select firstname, surname, hours, rank() over (order by hours desc) from
 	(select firstname, surname,
-		((sum(bks.slots)+10)/20)*10 as hours
+		round(sum(bks.slots) / 2, -1) as hours
 
 		from cd.bookings bks
 		inner join cd.members mems
